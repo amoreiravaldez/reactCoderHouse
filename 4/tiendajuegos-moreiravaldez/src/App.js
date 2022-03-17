5780115e13996633178383a1cefacbd74e759059
Alvaro Moreira <amoreiravaldez@gmail.com>
2022-03-16 23:15:01 -0300

    Cambios clase 4

---
+++ 4/tiendajuegos-moreiravaldez/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './component/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
